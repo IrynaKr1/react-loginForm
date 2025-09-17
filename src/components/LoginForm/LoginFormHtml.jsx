0a4e865b09f525ad31ae527c8d4056da50487217
IrynaKr1 <krutsiryna@gmail.com>
2025-09-17 19:29:39 +0200

    Add form styling

--- src/components/LoginForm/LoginFormHtml.jsx
+++ src/components/LoginForm/LoginFormHtml.jsx
@@ -5,37 +5,57 @@ import styles from './LoginForm.module.css';
 function LoginFormHtml(props) {
   return (
     <>
-      <div className='formContainer'>
-        <div className='imageWrapper'>
-          <img src={smileBeamIcon} alt='Smile face icon' />
+      <div className={styles.formContainer}>
+        <div className={styles.imageWrapper}>
+          <img
+            src={smileBeamIcon}
+            alt='Smile face icon'
+            className={styles.headerImg}
+          />
         </div>
-        <h1>Create Your Account</h1>
-        <form>
-          <label>
-            <span>Full Name</span>
+        <h1 className={styles.formHeader}>Create Your Account</h1>
+        <form className={styles.loginForm}>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Full Name</span>
             <input
               type='text'
               name='fullName'
               placeholder='John Doe'
               autoFocus
+              className={styles.input}
             />
           </label>
-          <label>
-            <span>Email address</span>
-            <input type='email' name='email' placeholder='johndoe@gmail.com' />
+          <label className={styles.label}>
+            <span className={styles.inputName}>Email address</span>
+            <input
+              type='email'
+              name='email'
+              placeholder='johndoe@gmail.com'
+              className={styles.input}
+            />
           </label>
-          <label>
-            <span>Password</span>
-            <input type='password' name='password' />
-            <button type='button'>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Password</span>
+            <input type='password' name='password' className={styles.input} />
+            <button type='button' className={styles.showPassword}>
               <img src={eyeIcon} alt='Show password button' />
             </button>
           </label>
-          <label>
-            <input type='checkbox' name='termsCheckbox' />
-            <span>I Agree all Statements in Terms od Service</span>
+          <label className={styles.label}>
+            <span className={styles.checkboxField}>
+              <input
+                type='checkbox'
+                name='termsCheckbox'
+                className={styles.checkbox}
+              />
+              <span className={styles.checkboxText}>
+                I Agree all Statements in Terms od Service
+              </span>
+            </span>
           </label>
-          <button type='submit'>Sign Up</button>
+          <button type='submit' className={styles.submitBtn}>
+            Sign Up
+          </button>
         </form>
         <p>
           I'm already a member!
